6bbeeeaf951e4f2e1060afbd73572a17c113fa20
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-04-05 12:10:52 -0400

    setting up app

---
+++ Compine/CompineApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompineApp.swift
+//  Compine
+//
+//  Created by asmaa gamal  on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CompineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Compine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Compine
+//
+//  Created by asmaa gamal  on 05/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
